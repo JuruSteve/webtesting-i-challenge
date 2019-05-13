ae64e68d8df569845fb1f3320661af993f2d4ce5
jurusteve <jstevemu17@gmail.com>
2019-05-13 18:09:27 -0400

    completed mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,18 +2,37 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
+  const newItem =
+    item.enhancement == 20 ? item.enhancement : (item.enhancement = 1);
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability = item.durability - 5;
+  } else {
+    if (item.enhancement > 15) {
+      item.durability = item.durability - 10;
+      item.enhancement = item.enhancement;
+    }
+    if (item.enhancement > 16) {
+      item.enhancement = item.enhancement - 1;
+    }
+  }
+
+  // if (item.enhancement > 16) {
+  //   item.enhancement = item.enhancement - 1;
+  // }
+
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,69 @@
-const enhancer = require('./enhancer.js');
+// @ts-nocheck
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("repair(item)", () => {
+  it("restores durability to 100", () => {
+    const actual = {
+      name: "item1",
+      durability: 0,
+      enhancement: 0
+    };
+    const expected = {
+      name: "item1",
+      durability: 100,
+      enhancement: 0
+    };
+    expect(enhancer.repair(actual)).toEqual(expected);
+  });
+});
+
+describe("success(item)", () => {
+  describe("enhances the new item object", () => {
+    describe("enhancement success", () => {
+      it("increases enhancement by 1", () => {
+        const actual = {
+          name: "item2",
+          durability: 0,
+          enhancement: 0
+        };
+        const expected = {
+          name: "item2",
+          durability: 0,
+          enhancement: 1
+        };
+        expect(enhancer.succeed(actual)).toEqual(expected);
+      });
+      it("keeps the same enhancement level", () => {
+        const actual = {
+          name: "item2",
+          durability: 0,
+          enhancement: 20
+        };
+        const expected = {
+          name: "item2",
+          durability: 0,
+          enhancement: 20
+        };
+        expect(enhancer.succeed(actual)).toEqual(expected);
+      });
+    });
+    describe("enhancement failure", () => {
+      it("decreases durability by 5 if enhancement is less than 15", () => {
+        const actual = { name: "item3", durability: 50, enhancement: 13 };
+        const expected = { name: "item3", durability: 45, enhancement: 13 };
+        expect(enhancer.fail(actual)).toEqual(expected);
+      });
+      it("decreases durability by 10 if enhancement is greater than 15", () => {
+        const actual = { name: "item3", durability: 50, enhancement: 18 };
+        const expected = { name: "item3", durability: 40, enhancement: 17 };
+        expect(enhancer.fail(actual)).toEqual(expected);
+      });
+      it("decreases enhancement by 1 if enhancement is greater than 16", () => {
+        const actual = { name: "item4", durability: 50, enhancement: 18 };
+        const expectedItem = { name: "item4", durability: 40, enhancement: 17 };
+        expect(enhancer.fail(actual)).toEqual(expectedItem);
+      });
+    });
+  });
+});
